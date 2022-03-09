a656b5895311aeffc84a93095f7296a179261eb8
Rishab42 <rishab0197@gmail.com>
2022-03-09 21:54:58 +0530

    add carousel and few changes in Ui

--- src/comp/Home.js
+++ src/comp/Home.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { useNavigate } from "react-router-dom";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import './home.css';
+
 
 function Home(){
 
@@ -26,6 +30,15 @@ function Home(){
     function account(){
         navigate("/Account");
     }
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 3
+      };
+
     return(
         <div className="home-container">
             <div className="navbar">
@@ -39,48 +52,75 @@ function Home(){
                     <span onClick={service}>Services</span>
                     <span onClick={contact}>Contact Us</span>
                     <span onClick={account}>My Account</span>
-                </div>    
+                </div>
             </div>
             <div className="box-image">
-                <img src="/boximg.jpg" />
-                <div className="box-text">
-                    <span className="green-text">B</span>
-                    <span>OX</span>
-                    <span className="green-text"> S</span>
-                    <span>TORE</span>
-                </div>
-                <div className="discription-text">
-                    <p>BoxUp offers high- quality, custom-printed boxes at Online.</p>
-                    
-                </div>
-                <div className="create-btn">
-                <button>Start Creating</button>
-                </div>
+                <div className='text-cont'>
+                    <div className="box-text">
+                        <span className="green-text">B</span>
+                        <span>OX</span>
+                        <span className="green-text"> S</span>
+                        <span>TORE</span>
+                    </div>
+                    <div className="discription-text">
+                        <p>BoxUp offers high- quality, custom-printed boxes at Online.</p>
+                    </div>
+                    <div className="create-btn">
+                        <button>Start Creating</button>
+                    </div>
+                </div>    
                 <div className="bottom-text">
                     <p>Make an instant impact with custom printed boxes that tell your brand story.</p>
-                </div>
-                {/* <div className="cards-block">
-                    <div className="card-text">
-                        <p>Shop All Boxes</p>
-                    </div>
-                    <div className="cards">
-                        <div className="card1">
-                            <img src="/card1.jpeg" />
-                        </div>
-                        <div className="card2">
-                            <img src="/card2.jpeg" />
-                        </div>
-                        <div className="card3">
-                            <img src="/card3.jpeg" />
-                        </div>
-                        <div className="card4">
-                            <img src="/card4.jpeg" />
-                        </div>
-
-                    </div>
-
-                </div> */}
+                </div>    
             </div>
+
+            <div className="shop-text">
+                <p>Shop All Boxes</p>
+            </div>
+            <div className="mul-cards">
+                <div className="card1">
+                </div>
+
+                <div className="card2">
+                </div>
+
+                <div className="card3">
+                </div>
+
+                <div className="card4">
+                </div>
+            </div>
+
+            <div className="product-carousel-container">
+                <div className="cust-text">
+                    <p> Customer Favourites</p>
+                </div>
+                <div className="carousel">
+                    <Slider {...settings}>
+                        <div className="car-img">
+                            <img src="./carr.jpg" />
+                        </div>
+                        <div className="car-img">
+                            <img src="./carr.jpg" />
+                        </div>
+                        <div className="car-img">
+                            <img src="./carr.jpg" />
+                        </div>
+                        <div className="car-img">
+                            <img src="./carr.jpg" />
+                        </div>
+                        <div className="car-img">
+                            <img src="./carr.jpg" />
+                        </div>
+                        <div className="car-img">
+                            <img src="./carr.jpg" />
+                        </div>
+                    </Slider>    
+                </div>
+            </div>
+            <div className='bottom'>
+            </div>
+
 
 
 
